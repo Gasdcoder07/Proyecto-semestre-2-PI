014816fd502652ace3f6b8dbaf0a478eadff0d37
Gasdcoder07 <gserna@ucol.mx>
2026-02-07 19:18:39 -0600

    Agregué carpeta de backend con django

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Hola ,{count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> sdasdssdasdcosa debe tener su propia cosa
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Hola papus :v  
       </p>
     </>
   )
