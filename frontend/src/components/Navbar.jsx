9380c0b2a14d06d9ae682bff48c642b986cb2cb3
Gasdcoder07 <gserna@ucol.mx>
2026-02-14 22:31:31 -0600

    Nueva rama de Greco Serna

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "../components/Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo">ManzaLife</div>
+            <ul className="nav-links">
+                <li><a>Login</a></li>
+                <li><a>Home</a></li>
+                <li><a>Account</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Home() {
+    return (
+        <div className="Homepage">
+             <h1>Bienvenido a ManzaLife</h1> 
+            <p>Un sitio web de sitios turísticos en Manzanillo.</p>
+        </div>
+    )
+}
+
