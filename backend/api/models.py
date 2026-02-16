63c9798436519bff6791ee3c3e9304d7876e8a2f
Gasdcoder07 <gserna@ucol.mx>
2026-02-16 16:56:54 -0600

    Implementación inicial del backend, un login masomenos y añadí Pillow a las librerías usadas en el backend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Nuestros modelos irán a partir de aquí:
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    location = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="posts/", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'post')
