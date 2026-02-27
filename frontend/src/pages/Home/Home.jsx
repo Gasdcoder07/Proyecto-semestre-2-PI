74fe08f013ab568c4194c711833df6b3e651edbd
Gasdcoder07 <gserna@ucol.mx>
2026-02-27 10:54:20 -0600

    Primer fetch a la API

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,18 @@
 import { FaSearch } from "react-icons/fa"
+import { useState, useEffect } from "react"
+
 
 export default function Home() {
+
+    const [publicacion, setPub] = useState([])
+
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/api/posts/")
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(err => console.error(err))
+    }, [])
+
     return (
         <div className="flex flex-col gap-4 text-white">
             <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">Bienvenido a Manza<span className="text-transparent bg-clip-text bg-linear-to-r from-orange-500 to-orange-800">Life</span></h1>
@@ -16,6 +28,14 @@ export default function Home() {
                     </input>
 
                     <FaSearch className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer hover:text-white transition-colors ease-in-out duration-200"/>
+                    {
+                        publicacion.map(post => (
+                            <div key={post.title} className="bg-white/10 p-4 rounded-xl">
+                                <h2 className="text-2xl font-bold">{post.title}</h2>
+                                <p>{post.content}</p>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </div>
