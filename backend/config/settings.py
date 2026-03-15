1a147dd63076bdbf62546fa6d1c687395fbb0252
Gasdcoder07 <gserna@ucol.mx>
2026-03-14 19:18:33 -0600

    Añadí supabase al proyecto, configuración se settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 """
 Django settings for config project.
 
@@ -88,10 +92,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #    )
 #}
 
+
+# postgresql://postgres:[YOUR-PASSWORD]@db.eoajugkucnfoesuuiznu.supabase.co:5432/postgres
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
