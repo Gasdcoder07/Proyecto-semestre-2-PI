48eac3e3bb29a8f4e843867c158cbb5639aa7a94
Valent-x7 <valentinvacacipres@gmail.com>
2026-03-18 09:12:41 -0600

    Fix formatting and improve readability in axios.js.

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://127.0.0.1:8000/api/",
@@ -7,19 +7,59 @@ const api = axios.create({
     }
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
 
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+
+            originalRequest._retry = true;
+
+            const refreshToken = localStorage.getItem("refreshToken");
+
+            if (!refreshToken) {
+                return Promise.reject(error);
+            }
+
+            try {
+
+                const res = await axios.post(
+                    "http://127.0.0.1:8000/api/token/refresh/",
+                    { refresh: refreshToken }
+                );
+
+                const newAccessToken = res.data.access;
+
+                localStorage.setItem("token", newAccessToken);
+
+                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+                return api(originalRequest);
+
+            } catch (err) {
+
+                localStorage.removeItem("token");
+                localStorage.removeItem("refreshToken");
+
+                return Promise.reject(err);
+            }
         }
 
-        return config;
-    },
-    (error) => {
         return Promise.reject(error);
     }
-)
+);
 
-export default api
+export default api;
