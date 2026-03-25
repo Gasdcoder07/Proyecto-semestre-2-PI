c448be7ea777517c7445a72b6602e02780a00f67
bryan <bsebastian0@ucol.mx>
2026-03-25 11:37:24 -0600

    hacer modificaciones el el fronted de BlogCategoriesCard.jsx y crear el archivo BlogCategoriesSkeleton.jsx

--- frontend/src/components/Blog/BlogCategories/BlogCategoriesCard.jsx
+++ frontend/src/components/Blog/BlogCategories/BlogCategoriesCard.jsx
@@ -1,22 +1,22 @@
 const BlogCategoriesCard = ({ nombre, imagen }) => {
   return (
-    <div className="w-full h-40 flex border border-neutral-800 rounded-xl overflow-hidden">
+    <div className="group w-full h-40 flex border border-neutral-800 rounded-xl overflow-hidden hover:border-neutral-700 hover:-translate-y-0.5 cursor-pointer transition-all duration-200 ease-in-out">
       
-      <div className="w-1/2 h-full">
+      <div className="w-1/2 h-full overflow-hidden">
         <img
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover group-hover:scale-105 transition-transform duration-500"
           src={imagen}
-          alt={nombre} />
+          alt={nombre} 
+        />
       </div>
 
-        <div className="w-1/2 px-8 py-4">
-          <p>
-            {nombre}
-          </p>
-        </div>
+      <div className="w-1/2 px-8 py-4 flex items-center">
+        <p className="font-medium tracking-widest group-hover:text-orange-600 transition-all duration-200 ease-in-out">
+          {nombre}
+        </p>
+      </div>
 
     </div>
-
   );
 };
 
---
+++ frontend/src/components/Blog/BlogCategories/BlogCategoriesSkeleton.jsx
@@ -0,0 +1,22 @@
+const BlogCategoriesSkeleton = () => {
+  return (
+    <>
+        <div className="mt-4 ease-in-out animate-pulse">
+            <div className="rounded bg-white/10 w-1/2 h-8"/>
+        </div>
+        <div className="py-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {
+                [1, 2, 3, 4, 5, 5].map((i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="h-32 rounded-xl bg-white/10 ease-in-out animate-pulse"/>
+                    )
+                })
+            }
+        </div>
+    </>
+  );
+};
+
+export default BlogCategoriesSkeleton;
