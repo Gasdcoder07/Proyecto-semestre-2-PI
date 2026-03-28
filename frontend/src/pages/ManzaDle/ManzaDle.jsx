768626bd4736c3a5133ceb990d6778b02458b5f4
Gasdcoder07 <gserna@ucol.mx>
2026-03-28 17:29:38 -0600

    Agregué los componentes del manzadle

--- frontend/src/pages/ManzaDle/ManzaDle.jsx
+++ frontend/src/pages/ManzaDle/ManzaDle.jsx
@@ -7,12 +7,27 @@ import Keyboard from "./Keyboard"
 import GameModal from "./GameModal"
 
 export default function ManzaDle() {
+
+    const [isGameOver, setIsGameOver] = useState(false)
+
     return (
         <div className="min-h-screen bg-linear-to-b from-zinc-950 to-orange-950 text-white flex flex-col font-sans">
             <Navbar />
-            <div className="grow flex flex-col items-center pt-28 pb-8 px-4 w-full max-w-lg mx-auto relative">
-                <Board />
-            </div>
+            <main className="grow flex flex-col items-center pt-28 pb-8 px-4 w-full max-w-lg mx-auto relative">
+                <Header />
+                <div className="w-full grow flex flex-col justify-center items-center mb-6">
+                    <Board />
+                </div>
+                
+                <div className="w-full">
+                    <Keyboard />
+                </div>
+                {isGameOver && (
+                    <div className="absolute inset-0 z-20 flex items-center justify-center bg-zinc-950/80">
+                        <GameModal />
+                    </div>
+                )}
+            </main>
         </div>
     )
 }
