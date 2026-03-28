1d1850b7c41adb9ff8ab2f896bdcaee873146a5a
Gasdcoder07 <gserna@ucol.mx>
2026-03-28 12:17:51 -0600

    Mejoras al Home

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -18,19 +18,19 @@ function Header () {
           <div className="absolute inset-0 bg-black/60 -z-20" />
           <div className="absolute inset-0 bg-linear-to-b from-transparent to-zinc-950 -z-10" />
 
-          <div className="text-white flex flex-col items-center gap-4 px-4">
-              <h1 className="text-4xl sm:text-6xl lg:text-8xl text-center tracking-wide">
+          <div className=" flex flex-col items-center gap-4 px-4">
+              <h1 className="text-white text-4xl sm:text-6xl lg:text-8xl text-center tracking-wide">
                   Manza
                   <span className="text-transparent bg-clip-text bg-linear-to-r from-orange-500 to-orange-800">
                       Life
                   </span>
               </h1>
 
-              <h3 className="text-xl sm:text-2xl lg:text-3xl text-center tracking-wider">
+              <h3 className="text-xl text-white/70 sm:text-2xl lg:text-3xl text-center tracking-wider">
                   Un lugar paradisíaco en las costas de México
               </h3>
 
-              <div className="relative w-full max-w-4xl text-white mt-2">
+              {/* <div className="relative w-full max-w-4xl text-white mt-2">
                   <input
                       className="bg-white/20 border border-white rounded-xl w-full py-3 pl-5 pr-10 outline-none placeholder-white/50"
                       type="text"
@@ -38,7 +38,7 @@ function Header () {
                   ></input>
 
                   <FaSearch className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer hover:text-orange-500 hover:rotate-45 transition-all ease-in-out duration-200" />
-              </div>
+              </div> */}
 
           </div>
       </section>
